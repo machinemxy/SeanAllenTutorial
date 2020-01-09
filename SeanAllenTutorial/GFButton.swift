d5f4daa592e8a10b87fb2c1383229f7b9f8722b8
Ma Xueyuan <machinemxy@me.com>
2020-01-09 23:14:49 +0900

    Finished GFButton and GFTextField

---
+++ SeanAllenTutorial/GFButton.swift
@@ -0,0 +1,36 @@
+//
+//  GFButton.swift
+//  SeanAllenTutorial
+//
+//  Created by Ma Xueyuan on 2020/01/09.
+//  Copyright © 2020 Ma Xueyuan. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
+}
