01047350bf32ec1d2f9393062e094a503b987888
Ma Xueyuan <machinemxy@me.com>
2020-02-04 01:26:55 +0900

    complete the follower cell

---
+++ SeanAllenTutorial/Views/Cells/FollowerCell.swift
@@ -0,0 +1,46 @@
+//
+//  FollowerCell.swift
+//  SeanAllenTutorial
+//
+//  Created by Ma Xueyuan on 2020/02/04.
+//  Copyright © 2020 Ma Xueyuan. All rights reserved.
+//
+
+import UIKit
+
+class FollowerCell: UICollectionViewCell {
+    static let reuseID = "FollowerCell"
+    
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+}
---
+++ SeanAllenTutorial/Views/GFAvatarImageView.swift
@@ -0,0 +1,29 @@
+//
+//  GFAvatarImageView.swift
+//  SeanAllenTutorial
+//
+//  Created by Ma Xueyuan on 2020/02/04.
+//  Copyright © 2020 Ma Xueyuan. All rights reserved.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    let placeholderImage = UIImage(named: "avatar-placeholder")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
