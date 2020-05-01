1c66a9d37243836fd882d005c799e9810eca3380
Ma Xueyuan <machinemxy@me.com>
2020-05-01 21:06:33 +0900

    finish the network part of getting user

--- SeanAllenTutorial/CustomThings/ViewControllers/UserInfoVC.swift
+++ SeanAllenTutorial/CustomThings/ViewControllers/UserInfoVC.swift
@@ -17,6 +17,15 @@ class UserInfoVC: UIViewController {
         view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
+        
+        NetworkManager.shared.getUserInfo(for: username) { [unowned self] result in
+            switch result {
+            case .success(let user):
+                print(user)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
     
 
