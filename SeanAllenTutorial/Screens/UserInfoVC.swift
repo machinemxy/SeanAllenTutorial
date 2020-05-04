0f6fbcfa3f3935403adf94c97554880133b8d52f
Ma Xueyuan <machinemxy@me.com>
2020-05-05 00:23:58 +0900

    Finished header part of UserInfoView

---
+++ SeanAllenTutorial/Screens/UserInfoVC.swift
@@ -0,0 +1,59 @@
+//
+//  UserInfoVC.swift
+//  SeanAllenTutorial
+//
+//  Created by Ma Xueyuan on 2020/04/14.
+//  Copyright © 2020 Ma Xueyuan. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    let headerView = UIView()
+    
+    var username: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        layoutUI()
+        
+        NetworkManager.shared.getUserInfo(for: username) { [unowned self] result in
+            switch result {
+            case .success(let user):
+                DispatchQueue.main.async {
+                    self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+                }
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
+    
+    func layoutUI() {
+        view.addSubview(headerView)
+        
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: 180)
+        ])
+    }
+    
+    func add(childVC: UIViewController, to containerView: UIView) {
+        addChild(childVC)
+        containerView.addSubview(childVC.view)
+        childVC.view.frame = containerView.bounds
+        childVC.didMove(toParent: self)
+    }
+
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+}
---
+++ SeanAllenTutorial/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SeanAllenTutorial
+//
+//  Created by Ma Xueyuan on 2020/05/05.
+//  Copyright © 2020 Ma Xueyuan. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
