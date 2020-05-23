a6007361b4247807223be995ec77a9bc280f9696
Ma Xueyuan <machinemxy@me.com>
2020-05-24 00:09:24 +0900

    finished PersistenceManager

--- SeanAllenTutorial/Screens/FollowerListVC.swift
+++ SeanAllenTutorial/Screens/FollowerListVC.swift
@@ -45,6 +45,9 @@ class FollowerListVC: UIViewController {
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.rightBarButtonItem = addButton
     }
     
     func configureCollectionView() {
@@ -105,6 +108,10 @@ class FollowerListVC: UIViewController {
         DispatchQueue.main.async {
             self.dataSource.apply(snapshot, animatingDifferences: true, completion: nil)
         }
+    }
+    
+    @objc func addButtonTapped() {
+        
     }
 }
 
